3bc066220d8504984c9e03be06fa320ad751fd01
dnwn306 <dnwn306@naver.com>
2021-12-05 03:39:50 +0900

    2021-12-05

--- 동전 0.py
+++ 동전 0.py
@@ -2,26 +2,20 @@ N, K = map(int, input().split())
 	
 coins = []
 count = 0
-idx = 0
 for _ in range(N):
 	coins.append( int( input() ) )
 
-coins.sort(reverse=True)
-size = len(coins)
-A = coins[:size//2:]
-B = coins[size//2::]
-
-print(A, B)
-
-while K > 0:
+idx = len( coins ) - 1
 	
-	if ( K - B[idx] ) >= 0:
-		K -= B[idx]
-		idx = 0
+while K > 0:
+
+	if ( K - coins[idx] ) >= 0 :
 		count += 1
+		K -= coins[idx]
+		idx = len(coins) - 1
+		continue
 		
-		
-	idx += 1
+	idx -= 1
 
 		
 print( count )
