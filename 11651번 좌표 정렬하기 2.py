004823826706829895e86b44abfd21af25838887
dnwn306 <dnwn306@naver.com>
2021-12-05 07:37:40 +0900

    2021-12-05

---
+++ 11651번 좌표 정렬하기 2.py
@@ -0,0 +1,12 @@
+N = int( input() )
+
+arr = []
+
+for _ in range(N):
+	arr.append( list( map(int, input().split()) ) )
+	
+
+arr.sort( key=lambda x: (x[1], x[0]) )
+
+for i in arr:
+	print( i[0], i[1] )
