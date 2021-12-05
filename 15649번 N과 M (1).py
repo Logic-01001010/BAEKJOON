c232a5f282b5b52850bae43795e3765c8d196634
dnwn306 <dnwn306@naver.com>
2021-12-05 10:07:23 +0900

    2021-12-05

---
+++ 15649번 N과 M (1).py
@@ -0,0 +1,9 @@
+from itertools import permutations
+
+n, m = map( int, input().split() )
+arr = list( permutations( range(1, n+1) , m) )
+
+for i in arr:
+	for j in i:
+		print(j,"", end='')
+	print()
